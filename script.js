cd9bd314f056cd1726e4938a9ebd6caec3c77612
Martina <bellerophons-pegasus@users.noreply.github.com>
2019-02-07 21:31:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-    $().ready(function(){
-        $.getJSON( "/test.json", function( data ) {
-        console.log(data);
-        $("#text").html(data["squadName"]);
-      });
-    });
-
-/*
-      var header = document.querySelector('header');
-      var section = document.querySelector('section');
-
-      var requestURL ='https://github.com/bellerophons-pegasus/lobib/raw/master/test.json';
-      var request = new XMLHttpRequest();
-      request.open('GET', requestURL);
-      request.responseType = 'json';
-      request.send();
-      
-      request.onload = function() {
-        var superHeroes = request.response;
-        populateHeader(superHeroes);
-        showHeroes(superHeroes);
-      }
-      
-      function populateHeader(jsonObj) {
-        var myH1 = document.createElement('h1');
-        myH1.textContent = jsonObj['squadName'];
-        header.appendChild(myH1);
-
-        var myPara = document.createElement('p');
-        myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
-        header.appendChild(myPara);
-      }
-      */
+    var header = document.querySelector('header');
+    var section = document.querySelector('section');
+  
+    var requestURL = 'https://bellerophons-pegasus.github.io/lobib/test.json';
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    
+    request.onload = function() {
+      var superHeroes = request.response;
+      populateHeader(superHeroes);
+      showHeroes(superHeroes);
+    }
+    
+    function populateHeader(jsonObj) {
+      var myH1 = document.createElement('h1');
+      myH1.textContent = jsonObj['squadName'];
+      header.appendChild(myH1);
+      var myPara = document.createElement('p');
+      myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
+      header.appendChild(myPara);
+    }
