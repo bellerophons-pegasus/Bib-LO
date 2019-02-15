67cc5da934d2b9bb663e6f000c74e0a1dd1d54b6
styx-im-hades <bellerophons_pegasus@yahoo.de>
2019-02-15 18:07:13 +0100

    Styling, wording etc.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
     var header = document.querySelector('header');
     var section = document.querySelector('section');
-  
+
     var requestURL = 'https://bellerophons-pegasus.github.io/lobib/test.json';
     var request = new XMLHttpRequest();
     request.open('GET', requestURL);
     request.responseType = 'json';
     request.send();
-    
+
     request.onload = function() {
       var glypticLiterature = request.response;
       showLiterature(glypticLiterature);
@@ -14,19 +14,20 @@
 
 function showLiterature(jsonObj) {
   var litlist = jsonObj['literature'];
-      
+
   for (var i = 0; i < litlist.length; i++) {
     var bibReference = document.createElement('div');
     bibReference.className = "referenceEntry";
     var myH2 = document.createElement('h2');
     var myPara1 = document.createElement('p');
-    var myPara2 = document.createElement('p');
+    var keyList = document.createElement('div');
+    keyList.className = 'keywords';
     var myList = document.createElement('ul');
 
     myH2.textContent = litlist[i].date;
-    myPara1.textContent = 'Title: ' + litlist[i].workLabel;
-    myPara2.textContent = 'topics: ' + litlist[i].topics;
-        
+    myPara1.textContent = litlist[i].workLabel;
+    keyList.textContent = litlist[i].topics;
+
 /*    var superPowers = litlist[i].powers;
     for (var j = 0; j < superPowers.length; j++) {
       var listItem = document.createElement('li');
@@ -37,7 +38,7 @@ function showLiterature(jsonObj) {
 
     bibReference.appendChild(myH2);
     bibReference.appendChild(myPara1);
-    bibReference.appendChild(myPara2);
+    bibReference.appendChild(keyList);
     bibReference.appendChild(myList);
 
     section.appendChild(bibReference);
